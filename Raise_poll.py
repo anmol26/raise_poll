a28626cfb486724904c17a9b6680e01eb4834ceb
Anmol Khandelwal <45265490+anmol26@users.noreply.github.com>
2021-09-21 10:03:33 +0530

    Update Raise_poll.py

--- Raise_poll.py
+++ Raise_poll.py
@@ -1,13 +1,13 @@
 
 poll_msg= input("Enter your poll message: ")             #Chai Break time??
-options= int(input("How many options are there? "))      #2
+options= int(input("How many options are there? "))      #3
 opt={}
 for i in range(1,options+1):
-    opt[i]=input("Enter Option "+ str(i)+": ")           #{1:"yes", 2:"no"}
+    opt[i]=input("Enter Option "+ str(i)+": ")           #{1:"yes", 2:"no", 3:"may be"}
     
 total=int(input("How many members are there? "))         #10
 
-votes=dict.fromkeys(range(1,options+1),0)                #{1:0,2:0}
+votes=dict.fromkeys(range(1,options+1),0)                #{1:0,2:0,3:0}
 #print(votes)
 names={}
 print("    !!! Let's start voting !!!   ")
